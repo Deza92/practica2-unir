eb539ee5745d0eb8129812aeafa0e14a468f793b
Deza92 <carloscastillodeza@gmail.com>
2023-03-17 09:08:23 +0100

    Edit Resources

--- terraform/recursos.tf
+++ terraform/recursos.tf
@@ -67,7 +67,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   location            = azurerm_resource_group.rg.location
   size                = "Standard_F2"
   admin_username      = "azureuser"
-  disable_password_authentication = "true"
+  disable_password_authentication = true
   network_interface_ids = [
     azurerm_network_interface.nic.id,
   ]
@@ -85,8 +85,12 @@ resource "azurerm_linux_virtual_machine" "vm" {
   }
 
   os_profile {
-    computer_name  = "hotstname"
-    admin_username = "adminname"
+    linux_configuration {
+      computer_name_prefix = "myPrefix"
+      admin_username       = "myadmin"
+
+      disable_password_authentication = false
+    }
   }
 }
 
