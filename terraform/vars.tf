84acbfd8fed9a4f2ba4900af60107a0bbfd1226e
Deza92 <carloscastillodeza@gmail.com>
2023-03-18 12:57:39 +0100

    edit file vars

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -43,7 +43,7 @@ variable "user" {
 # Variable de la contraseña del usuario 
 variable "pass_user" {
   type = string
-  default = "passUnir_"
+  default = "passUnir1."
   description = "password usuario user"
 }
 
